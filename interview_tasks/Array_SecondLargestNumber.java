a05138ae48313fc19f7ed07e81a9069bb89b9f58
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-03-08 21:28:31 -0500

    Coding Task

--- interview_tasks/Array_SecondLargestNumber.java
+++ interview_tasks/Array_SecondLargestNumber.java
@@ -4,15 +4,12 @@ public class Array_SecondLargestNumber {
 
 
 
-    //solution 1
+
     public static int secondLargestNumber(int[] arr){
         return Arrays.stream(arr).filter( p -> p != Arrays.stream(arr).max().getAsInt()).max().getAsInt();
     }
 
-    //solution 2
-    public static int secondLargestNumber2(int[] arr){
-return 0;
-    }
+
 
 
 }
