f2c9eee03177e8f6a52e5b452b36d2c340256a64
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-03-10 15:41:12 -0500

    Coding Task

--- interview_tasks/Number_SumAllDigitsUntilOneDigit.java
+++ interview_tasks/Number_SumAllDigitsUntilOneDigit.java
@@ -6,32 +6,58 @@ public class Number_SumAllDigitsUntilOneDigit {
 
     /*
     Sum all digits of a number until you have one digit number
-    EX:45659=4+5+6+5+9=29=2+9=11=1+1=2
+    EX:
+        input: 45659
+        output:
+                29
+                11
+                2
+        Explanation:
+                4+5+6+5+9=29
+                2+9=11
+                1+1=2
      */
 
     public static void main(String[] args) {
 
         int number = 45659;
+        while(true){
+            int sum = 0;
+            while (number > 0) {
+                int digit = number % 10;
+                number /= 10;
+                sum += digit;
+            }
+            number = sum;
+            System.out.println(sum);
+            if(sum < 9){
+                break;
+            }
+        }
 
-
-
-        int sum = 0;
+        /*
+        int number = 45659;
         do{
+            int sum = 0;
             List<Integer> digits = new ArrayList<>();
-            while(number > 0){
+            while (number > 0) {
                 int digit = number % 10;
                 number /= 10;
                 digits.add(digit);
             }
             Collections.reverse(digits);
-
             for (Integer each : digits) {
                 sum += each;
             }
-            System.out.println(sum);
             number = sum;
-            digits.clear();
+            System.out.println(sum);
         }while(sum > 9);
+*/
+
+
+
+
+
 
 
 
